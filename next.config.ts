e27526f24a55e7f87b43f76c56c987cb17acf845
Aakash <176982645+Aakash0419@users.noreply.github.com>
2024-12-02 11:28:55 +0530

    forgot to add this in last commit

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images:{
-    dangerouslyAllowSVG:true,
+   // dangerouslyAllowSVG:true,
     remotePatterns:[
       {
       protocol:'https',
