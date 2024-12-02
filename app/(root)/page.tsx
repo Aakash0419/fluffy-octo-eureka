da7882241fa4cac687450e5decf0c543492291f1
Aakash <176982645+Aakash0419@users.noreply.github.com>
2024-12-02 15:38:26 +0530

    small changes  to main page and add env variable for the dataset and api version

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import Searchbar from '../components/Searchbar';
 
-export default function Home({searchParams}:{searchParams:{query:string}}) {
-  const query = searchParams?.query||'';
+export default async function Home({searchParams}:{searchParams:Promise<{query:string}>}) {
+  const query = (await searchParams)?.query||'';
   return (
     <>
       <section className="yellow_container">
